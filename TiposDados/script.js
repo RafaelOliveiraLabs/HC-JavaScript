2280e6e2ffe074a287e6bbfe2358a171ebc4bfde
rafamoliv <rafaoliv.ti@gmail.com>
2021-07-07 15:02:29 -0300

    tipagem

---
+++ TiposDados/script.js
@@ -0,0 +1,32 @@
+var myName = "Rafael"; //string
+var age = 23; //number
+var height = 1.58; //float
+
+//null objeto vazio
+//undefined objeto não definido
+
+var isStudent = false; //boolean
+
+//objeto
+var person = {
+  name: "Rafael",
+  age: 23,
+};
+
+//array
+var students = ["Rafael", "Simara", "Joao"];
+
+//function
+function sum(a, b) {
+  return a + b;
+}
+
+//operador typeof
+console.log(typeof 32);
+
+console.log(typeof isStudent);
+
+var object = null;
+console.log(typeof null);
+
+console.log(typeof sum);
