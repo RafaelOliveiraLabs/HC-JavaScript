476bbf3eb42638fffdd990b51deff15165404b1e
rafamoliv <rafaoliv.ti@gmail.com>
2021-07-07 15:11:02 -0300

    operadores

---
+++ Operadores/script.js
@@ -0,0 +1,54 @@
+//operadores aritmeticos
+
+// add +
+var sumTwoNumbers = 4 + 3;
+console.log(sumTwoNumbers);
+
+// sub -
+var subTwoNumbers = 4 - 3;
+console.log(subTwoNumbers);
+
+// mult *
+var multTwoNumbers = 4 * 3;
+console.log(multTwoNumbers);
+
+//div /
+var divTwoNumbers = 4 / 3;
+console.log(divTwoNumbers);
+
+//resto ou modulo de divisao %
+var restoTwoNumbers = 4 % 3;
+console.log(restoTwoNumbers);
+
+//incremento ++
+var num = 1;
+console.log(++num);
+
+//decremento --
+console.log(--num);
+
+//operadores de atribuicao
+// = atribuicao
+
+var x = 1;
+var y = 3;
+
+console.log((x += y));
+console.log((x -= y));
+console.log((x *= y));
+console.log((x /= y));
+console.log((x %= y));
+
+//operadores de comparacao
+
+//==
+//===
+//!=
+//< ou <=
+//> ou >=
+
+//operadores logicos
+
+// e &&
+// ou ||
+// negacao !
