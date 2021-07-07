a2cec1efea83cb4d5354d9a2c594dff30780a283
rafamoliv <rafaoliv.ti@gmail.com>
2021-07-07 15:38:47 -0300

    condicionais

---
+++ Condicionais/script.js
@@ -0,0 +1,28 @@
+/* var num = 1;
+num = 5;
+
+if (num === 1) {
+  console.log("num é igual a 1");
+} else {
+  console.log("num é diferente de 1");
+}
+ */
+
+var mes = "janeiro";
+
+switch (mes) {
+  case "fevereiro":
+    console.log("mes 2");
+    break;
+
+  case "marco":
+    console.log("mes 3");
+    break;
+
+  case "janeiro":
+    console.log("mes 1");
+    break;
+
+  default:
+    console.log("Qualquer outro mes");
+}
