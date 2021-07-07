fbe3f332179c3f90769b4081a84dcad2f7000ef1
rafamoliv <rafaoliv.ti@gmail.com>
2021-07-07 14:53:26 -0300

    variaveis

--- Variaveis/script.js
+++ Variaveis/script.js
@@ -1,7 +1,19 @@
-var myName = "Rafael";
+/* var myName = "Rafael";
 
 let language = "Javascript";
 
 const pattern = "ECMAScript";
 
 console.log(myName, language, pattern);
+ */
+
+//hoisting
+/* {
+  var age = 32;
+}
+
+console.log(age); */
+
+// somente inicia const com um valor atribuido
+const myLastName = "Oliveira";
+console.log(myLastName);
