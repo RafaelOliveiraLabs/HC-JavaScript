7286ee7c7300f13fdd467c60f26dd2ef5ae76104
rafamoliv <rafaoliv.ti@gmail.com>
2021-07-08 10:52:54 -0300

    arrowFunction

---
+++ ArrowFunction/arrowFunctions.js
@@ -0,0 +1,15 @@
+// function
+
+function soma(a, b) {
+  return a + b;
+}
+
+console.log(soma(3, 2));
+
+// arrow function
+
+const sum = (num1, num2) => num1 + num2;
+console.log(sum(3, 5));
+
+const sayHello = (name) => `Hello ${name}`;
+console.log(sayHello("Rafa"));
